7280eea65c0ff0e5fcb557597fd611f9f9707d84
wkflanders <wflan@seas.upenn.edu>
2025-12-31 20:53:00 -0500

    feat: polymarket constants

---
+++ pm/polymarket/constants.py
@@ -0,0 +1,9 @@
+DEFAULT_GAMMA_BASE_URL = "https://gamma-api.polymarket.com"
+DEFAULT_CLOB_BASE_URL = "https://clob.polymarket.com"
+DEFAULT_USER_AGENT = "pm/0.1"
+
+GAMMA_MARKETS_PATH = "/markets"
+GAMMA_EVENTS_PATH = "/events"
+
+CLOB_ORDERBOOK_PATH = "/book"
+CLOB_TRADES_PATH = "/trades"
